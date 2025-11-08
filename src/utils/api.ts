f2249876ed7568de319861239647553914aac236
iamshubham20 <shubham@helpfulgenie.app>
2025-11-08 15:17:54 +0530

    Add backend url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
   ? '/api' 
-  : 'http://localhost:3001/api';
+  : 'https://password-generator-secure-vault.onrender.com/api';
 
 export interface User {
   id: string;
